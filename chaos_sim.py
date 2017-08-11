b40aacbc395be4ecb5b01b4235901996c8bed729
Nash Keyes <ndbkeyes@gmail.com>
2017-08-10 23:32:07 -0400

    Detailed full values plot
    Took out code that attempted to pick out the final equilibria values. That didn't work well anyway and I wanted to see the fuller picture of what the whole space looks like. Also changed parameters in initial recursion and in r values plotted to make a more detailed image. WARNING - long run time!

--- chaos_sim.py
+++ chaos_sim.py
@@ -1,14 +1,17 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 # function used to apply to itself
 def func(x, r):
-    return r * (x - x * x)
+    return r * x * (1 - x)
 
-# single algorithm run
-# parameters: tuning parameter r, initial pop %, whether to print info (optional, default False)
-# return: number of equilibria of that equation
+"""
+algorithm that runs func(x,r) over and over on itself
+parameters: tuning parameter r, initial pop %, whether to print info (optional, default False)
+return: number of equilibria of that equation
+"""
 def recursion(r, x, info=False):
-    
+
     y_values = []
 
     # iterate function on itself
@@ -16,7 +19,7 @@ def recursion(r, x, info=False):
         x = func(x, r)  # set new x value to current y value
         y_values.append(x)
 
-    eqs = find_eqs(y_values)
+    eqs = y_values #find_eqs(y_values)
 
     # print and plot equilibria info
     if info:
@@ -27,79 +30,49 @@ def recursion(r, x, info=False):
 
     return eqs
 
-# search for equilibria in function values
-# parameters: array of function values, boolean of whether to print info (optional, default False)
-# return: array of equilibria in function values
+
+"""
+search for equilibria in function values
+parameters: array of function values, boolean of whether to print info (optional, default False)
+return: array of equilibria in function values
+"""
 def find_eqs(arr, info=False):
-    
+
     # dict holding [possible equilibrium value : number of occurrences]
     values = {}
-    
+
     # add new equilibria instances to list
     for i in range(1,len(arr)-1):
         rounded = round(arr[i], 4)  # round to 2 decimal places
         minimum = rounded < arr[i-1] and rounded < arr[i+1]  # check if local min
         maximum = rounded > arr[i-1] and rounded > arr[i+1]  # check if local max
-        if minimum or maximum:  # only add to equilibrium values if local min or max
-            if rounded not in values: 
-                values[rounded] = 1
-            else:
-                values[rounded] += 1
+        if rounded not in values:
+            values[rounded] = 1
+        else:
+            values[rounded] += 1
         i += 1
 
     # array holding recurring equilibrium values (n >= 10)
     eqs = []
 
-    # check to see which show up 
+    # check to see which show up
     for x in values:
         if info:
             print(x)
         if values[x] >= 10:
-            eqs.append(x)            
+            eqs.append(x)
 
     return eqs
-    
-# PROGRAM MAIN
-# parameter: pop - 0 to 1, percent of total possible population at start
-# return: y - array of #s of eqs for different values of r
-def main(pop):
 
-    # values of r to iterate over
-    r_arr = [round(n * 0.001,3) for n in range(2500,4001,1)]
 
-    # store x and y values to plot
-    x = []
-    y = []
-    
-    # run algo for different values of r
-    for r in r_arr:
-        run = len(recursion(r,pop))
-        x.append(r)
-        y.append(run)
-        print(r, run)
-
-    # plot # of eqs over values of r
-    plt.plot(x,y)
-    plt.show()
-
-    return y
-
-"""
-main(0.5)
-recursion(3.844,0.5,True)
-recursion(3.86,0.5,True)
-"""
-
-# recursion(2.9999999999999999999999, 0.5, True)
 # generate graph of equilibrium values vs values of r
 def plot(pop):
-    r_arr = [round(n * 0.001,3) for n in range(1000,4001,1)]
+    r_arr = [round(n * 0.0001,5) for n in range(10000,40001,1)]
 
     x = r_arr
     y = []
     for r in r_arr:
         value = recursion(r,pop)
-        # print(value)
         y.append(tuple(value))
 
     print(y)
@@ -107,7 +80,8 @@ def plot(pop):
     for xe, ye in zip(x, y):
         plt.scatter([xe] * len(ye), ye, s=0.5)
     plt.show()
+    print("end")
 
-    return -1
 
 plot(0.5)
+# recursion(3.544, 0.5, True)
